26744a8f0d98d0a6ad884c6d2ab2daff75e69bb5
Justin Hager <coachjustinhager@gmail.com>
2020-11-06 12:03:15 -0800

    Updates

---
+++ precache-manifest.67066d105e869343bae472a0c978ce4f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe9d827234eaf9e2f84b63879de9236c",
+    "url": "/react-laptop-refactor/index.html"
+  },
+  {
+    "revision": "018d2764320384e95446",
+    "url": "/react-laptop-refactor/static/css/main.72a41afd.chunk.css"
+  },
+  {
+    "revision": "a3189dd90bd7726e1ec5",
+    "url": "/react-laptop-refactor/static/js/2.6549ce60.chunk.js"
+  },
+  {
+    "revision": "018d2764320384e95446",
+    "url": "/react-laptop-refactor/static/js/main.124fe6c9.chunk.js"
+  },
+  {
+    "revision": "ec9f30b9f6faaf7918b4",
+    "url": "/react-laptop-refactor/static/js/runtime~main.84b9e849.js"
+  }
+]);
